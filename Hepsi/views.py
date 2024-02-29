4a757cf2eda934fa9d66ff70c941a2ad5e727941
MelihOZKAS <melihozkass@gmail.com>
2024-03-01 01:53:19 +0300

    Nasip

--- Hepsi/views.py
+++ Hepsi/views.py
@@ -64,7 +64,7 @@ def home(request):
 
 
     title = "Çocuk Masalları | Uyku Masalı | Çocuk Hikayeleri | Masal Oku"
-    description = "Çocuk masalları ve çocuk hikayeleri sitemizde masal okuyabilir masal dinleyebilir, uyku masallarına bakabilirsiniz. Masallar, hikayeler hepsi eğitici sitemizde"
+    description = "Çocuklar için en güzel masallar, Uyku masalları, hayvan masalları, klasik masallar. Çocuğunuzun hayal gücünü geliştirecek masallarımızı keşfedin masal oku"
     keywords = "Çocuk Masalları, Çocuk Masalları Oku, Masallar, Masal Dinle, Kısa Masallar, Masal Oku, Masallar, Çocuk Hikayeleri, Eğitici Hikayeler, Klasik Masallar, Öğretici Masallar, Çocuklar için Masallar, Çocuklar için Hikayeler,Uyku masalları"
 
 
