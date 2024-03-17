edca46ad003ffb6aadb29f64e5a86dc271dbda6f
MelihOZKAS <melihozkass@gmail.com>
2024-03-17 13:42:36 +0300

    Nasip

--- Masallar/urls.py
+++ Masallar/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 from django.contrib.sitemaps.views import index, sitemap
 from .sitemaps import *
 from Hepsi.views import *
-from feed import *
+from .feed import *
 
 
 
