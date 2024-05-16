a949888b65b9eda66a8c174132217ea4bec8f8dc
MelihOZKAS <melihozkass@gmail.com>
2024-05-16 23:22:17 +0300

    Nasip

--- Masallar/urls.py
+++ Masallar/urls.py
@@ -3,14 +3,49 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
-
+from django.contrib.sitemaps.views import index, sitemap
+from .sitemaps import *
 from Hepsi.views import *
 from .feed import *
 
 
 
+sitemaps = {
+    'masal-kategorileri': MasalKategorileriSitemap,
+    'masallar': MasallarSitemap,
+    'hikaye-kategorileri': HikayeAltKategoriSitemap,
+    'hikayeler': HikayelerSitemap,
+    'cocuk-gelisimi': CocukSitemap,
+    'dini-masallari': DiniMasallariSitemap,
+    'peri-masallari': PeriMasallariSitemap,
+    'uyku-masallari': UykuMasallariSitemap,
+    'hayvan-masallari': HayvanMasallariSitemap,
+    'prenses-masallari': PrensesMasallariSitemap,
+    'prens-masallari': PrensMasallariSitemap,
+    'aile-masallari': AileMasallariSitemap,
+    'macera-masallari': MaceraMasallariSitemap,
+    'komik-masallari': KomikMasallariSitemap,
+    'egitici-masallari': EgiticiMasallariSitemap,
+    'arkadas-masallari': ArkadasMasallariSitemap,
+    'kardes-masallari': KardesMasallariSitemap,
+    'iyilik-masallari': iyilikMasallariSitemap,
+    'keloglan-masallari': KelOglanMasallariSitemap,
 
+    'dini-hikayeler': DiniHikayelerSitemap,
+    'sihirli-dunya-hikayeleri': SihirliDunyaHikayelerSitemap,
+    'sevimli-hayvan-hikayeleri': SevimliHayvanHikayelerSitemap,
+    'kahraman-hikayeleri': KahramanHikayelerSitemap,
+    'aile-hikayeleri': AileHikayelerSitemap,
+    'hazine-avi-hikayeleri': HazineHikayelerSitemap,
+    'eglenceli-yolculuk-hikayeleri': YolculukHikayelerSitemap,
+    'gezi-maceralari-hikayeleri': GeziHikayelerSitemap,
+    'uzay-maceralari-hikayeleri': UzayHikayelerSitemap,
+    'muhtesem-bilim-hikayeleri': BilimHikayelerSitemap,
+    'dostluk-hikayeleri': DostlukHikayelerSitemap,
+    'doga-ve-cevre-onemi-hikayeleri': DogaHikayelerSitemap,
+    'ingilizce-hikayeler': EnglishHikayelerSitemap,
+    'sevimli-canavar-hikayeleri': SevimliCanavarHikayelerSitemap,
+}
 
 def handler404(request, *args, **argv):
     response = render(request, '404.html')
@@ -19,8 +54,9 @@ def handler404(request, *args, **argv):
 
 urlpatterns = [
     path("yonetici/", admin.site.urls),
-
+    path('sitemap.xml/', index, {'sitemaps': sitemaps}),
     path("", include("Hepsi.urls")),
+    path('sitemap-<section>.xml/', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('feeds/dini-masallar/', DiniMasallarFeed(), name='dini_masal_feed'),
     path('feeds/peri-masallari/', PeriMasallariFeed(), name='peri_masal_feed'),
     path('feeds/uyku-masallari/', UykuMasallariFeed(), name='uyku_masallari_feed'),
