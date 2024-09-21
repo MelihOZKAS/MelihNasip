07de4553290362565ac8232921e950177a344dd8
MelihOZKAS <melihozkass@gmail.com>
2024-09-21 14:36:11 +0300

    Nasip

--- Hepsi/urls.py
+++ Hepsi/urls.py
@@ -37,8 +37,8 @@ urlpatterns = [
     path("masal-hikaye-ekle/", views.ekle, name="masal-hikaye-ekle"),
     path("api-masal-hikaye-ekle/", views.apiyle_ekle, name="api-masal-hikaye-ekle"),
 
-    path("cocuklara-matematik-oyunu", views.matematik, name="matematikoyunu"),
-    path("cocuklara-hayvan-oyunu", views.hayvanoyunu, name="hayvanoyunu"),
+    path("cocuklara-matematik-oyunu/", views.matematik, name="matematikoyunu"),
+    path("cocuklara-hayvan-oyunu/", views.hayvanoyunu, name="hayvanoyunu"),
     path("cocuk-hikayeleri/", views.Hikayeler, name="HikayelerHome"),
     # path("cocuk/", views.BlogHome, name="cocuk"),
     path("cocuk-gelisimi/", views.BlogHome, name="cocuk"),
